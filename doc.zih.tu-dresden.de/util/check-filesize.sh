01e25a3a111f93eb51088b8d45ac20489945f23a
Martin Schroschk <martin.schroschk@tu-dresden.de>
2022-09-07 12:17:41 +0200

    Clean up

--- doc.zih.tu-dresden.de/util/check-filesize.sh
+++ doc.zih.tu-dresden.de/util/check-filesize.sh
@@ -34,15 +34,10 @@
 large_files_present=false
 branch="origin/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-preview}"
 source_hash=`git merge-base HEAD "$branch"`
-
 lfsfiles=$(git lfs ls-files -n)
 
 for f in $(git diff $source_hash --name-only); do
     # Do not check size of git lfs files.
-    # This is a bit "Going around your elbow to get to your ear" by assuming that lfs files are
-    # ending with webm or mp4
-    #if [[ "$f" =~ .*".webm" ]] || [[ "$f" =~ .*".mp4" ]] ; then
-    echo $f
     if [[ $lfsfiles =~ "$f" ]]; then
         echo "Skip file ${f} in webm or mp4 format cause it is a git lfs file."
         continue
