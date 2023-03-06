641706e975f2e59fed210fde27f7e14a97d02b3c
Martin Schroschk <martin.schroschk@tu-dresden.de>
2023-03-06 13:45:02 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- doc.zih.tu-dresden.de/util/check-toc-equals-page-headings.py
+++ doc.zih.tu-dresden.de/util/check-toc-equals-page-headings.py
@@ -16,7 +16,6 @@ whitelist = ["index.md"]  # ["archive"]
 
 
 def get_heading_in_file(filename, docs_path):
-    # TODO join path filename
     # Read until first level one heading is found
     f = Path.joinpath(docs_path, filename)
     with open(f, "r") as file:
